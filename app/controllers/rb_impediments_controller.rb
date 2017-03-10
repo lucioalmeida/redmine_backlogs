e8a10f3d7c770c621e5f7a9b61bb09253f0d9277
lucioalmeida <luciocostaalmeida@gmail.com>
2017-03-10 19:26:37 -0300

    Update rb_impediments_controller.rb

--- app/controllers/rb_impediments_controller.rb
+++ app/controllers/rb_impediments_controller.rb
@@ -22,20 +22,16 @@ class RbImpedimentsController < RbApplicationController
   end
 
   def update
-    puts "update1"
     @impediment = RbTask.where(id: params[:id]).first
     @settings = Backlogs.settings
-    puts "update2"
     begin
       result = @impediment.update_with_relationships(params)
     rescue => e
       render :text => e.message.blank? ? e.to_s : e.message, :status => 400
       return
     end
-    puts "update3"
     status = (result ? 200 : 400)
     @include_meta = true
-    puts "update4"
     respond_to do |format|
       format.html { render :partial => "impediment", :object => @impediment, :status => status }
     end
